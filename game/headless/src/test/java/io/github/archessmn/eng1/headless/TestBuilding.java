45d485ba75871c21cfbd8ff7db26ba74783ad490
Letam <letam4nigeria@gmail.com>
2025-01-05 20:53:27 +0000

    Added buildingtype tests for Building class

---
+++ game/headless/src/test/java/io/github/archessmn/eng1/headless/TestBuilding.java
@@ -0,0 +1,24 @@
+package io.github.archessmn.eng1.headless;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.archessmn.eng1.World;
+import io.github.archessmn.eng1.buildings.Building;
+import io.github.archessmn.eng1.buildings.GymBuilding;
+
+public class TestBuilding extends AbstractHeadlessGdxTest {
+    @Test
+    public void TestCorrectBuildingType() {
+
+        World world = new World(100, 100); // use a test world.
+        GymBuilding gymBuilding = new GymBuilding(world); // Verify the building's type is set to GYM
+        assertEquals(Building.Type.GYM, gymBuilding.getBuildingType());
+        // when you have an instance of a GYM building
+        // it should be reflected in the getBuildingType() option
+        // if it isn't then this could affect the counters as it is not stored as the
+        // right building type
+
+    }
+}
