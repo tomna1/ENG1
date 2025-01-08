ee697a497deabe457e49091797fec1644dbf7fa5
tomna1 <thomas.e.nash@googlemail.com>
2025-01-08 15:56:24 +0000

    Added documentation for achievement package

--- game/core/src/main/java/io/github/archessmn/eng1/achievements/AbstractAchievement.java
+++ game/core/src/main/java/io/github/archessmn/eng1/achievements/AbstractAchievement.java
@@ -2,6 +2,9 @@ package io.github.archessmn.eng1.achievements;
 
 import io.github.archessmn.eng1.leaderboard.CompletedAchievement;
 
+/**
+ * All achievements should inherit from this class.
+ */
 public abstract class AbstractAchievement {
     private int id;     // Used by the manager to add and remove it from arrays.
     private String title;
@@ -9,10 +12,24 @@ public abstract class AbstractAchievement {
     private String iconPath;
     protected AchievementManager manager;
     
+    /**
+     * Creates a new achievement with the all the specified attributes.
+     * @param manager Reference to the manager that manages this achievement. 
+     * Cannot be null
+     * @param id The unique id of an achievement. Has to be unique in the 
+     * {@link AchievementManager}.
+     * @param title The title of the achievement. Cannot be null or empty
+     * @param description The description of the achievement (how it can be earned).
+     * Cannot be null or empty
+     * @param iconPath The path to the icon in the assets folder used to represent
+     * the achievement. Cannot be null.
+     */
     public AbstractAchievement(AchievementManager manager, int id, String title, String description, String iconPath) {
         if (manager == null) throw new IllegalArgumentException("Manager cannot be null");
-        if (title == null) throw new IllegalArgumentException("achievement cannot be null");
+        if (title == null) throw new IllegalArgumentException("title cannot be null");
+        if (title.equals("")) throw new IllegalArgumentException("title cannot be empty");
         if (description == null) throw new IllegalArgumentException("decription cannot be null");
+        if (description.equals("")) throw new IllegalArgumentException("description cannot be empty");
         if (iconPath == null) throw new IllegalArgumentException("iconPath cannot be null.");
         this.manager = manager;
         this.id = id;
@@ -21,31 +38,61 @@ public abstract class AbstractAchievement {
         this.iconPath = iconPath;
     }
 
+    /**
+     * Returns the unique id of the achievement.
+     * @return Id of achievement.
+     */
     public int getID() {
         return id;
     }
 
+    /**
+     * Returns the title associated with the achievement that was defined in the
+     * constructor. Wont be null or empty string.
+     * @return Title of achievement.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Returns the description of the achievement that was defined in the constructor.
+     * Wont be null or empty string.
+     * @return Description of achievement.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns the path to the icon in the assets folder used to represent the
+     * achievement. Wont be null or empty string.
+     * @return Icon path.
+     */
     public String getIconPath() {
         return iconPath;
     }
 
-    
+    /**
+     * Checks if the achievement has been achieved or not.
+     * @return true if has been achieved and false if not.
+     */
     public abstract boolean checkIfAchieved();
 
+    /**
+     * Returns the {@link CompletedAchievement} form of this achievement used
+     * by the leaderboard based on the title, description and icon path defined
+     * in the constructor.
+     * @return Completed achievement.
+     */
     public CompletedAchievement getCompletedAchievement() {
         return new CompletedAchievement(iconPath, title, description);
     }
 
     /**
-     * This method should be called when the achievement is completed.
+     * This method should be called when the achievement is completed. It will
+     * call the {@link AchievementManager#onAchievementCompletion(AbstractAchievement)}
+     * method with itself as the argument.
      */
     protected void onCompleted() {
         manager.onAchievementCompletion(this);
--- game/core/src/main/java/io/github/archessmn/eng1/achievements/AchievementManager.java
+++ game/core/src/main/java/io/github/archessmn/eng1/achievements/AchievementManager.java
@@ -7,7 +7,8 @@ import io.github.archessmn.eng1.WorldStats;
 import io.github.archessmn.eng1.leaderboard.CompletedAchievement;
 
 /**
- * Manages all achievements in the game.
+ * Manages all achievements in the game. Will create all the achievement in the
+ * game and will periodically check if they have been achieved or not.
  */
 public class AchievementManager {
     private WorldStats worldStats; // References to worldstats to make it easy to create achievements.
@@ -19,6 +20,14 @@ public class AchievementManager {
     private float timeBetweenChecks;
     private boolean isTimeChecksEnabled;
 
+    /**
+     * Creates a new AchievementManager with the parameters.
+     * @param worldStats A reference to the stats of the world used by some achievements.
+     * @param satisfactionStats A reference to the stats of satisfaction used by some achievements.
+     * @param timeBetweenChecks The time in seconds in between checks. For example if this was 1.0f,
+     * every 1 second the manager would forcefully check all achievements to see if they have been
+     * completed or not.
+     */
     public AchievementManager(WorldStats worldStats, SatisfactionStats satisfactionStats, float timeBetweenChecks) {
         if (worldStats == null) throw new IllegalArgumentException("World stats cannot be null");
         if (satisfactionStats == null) throw new IllegalArgumentException("satisfaction stats cannot be null");
@@ -87,10 +96,18 @@ public class AchievementManager {
         completedAchievements.add(achievement.getCompletedAchievement());
     }
 
+    /**
+     * Returns the list of all completed achievements throughout the game.
+     * @return List of completed achievements. Wont be null
+     */
     public ArrayList<CompletedAchievement> getCompletedAchievements() {
         return completedAchievements;
     }
 
+    /**
+     * Returns the amount of achievement that were completed throughout the game.
+     * @return
+     */
     public int getCompletedAchievementCount() {
         return completedAchievements.size();
     }
