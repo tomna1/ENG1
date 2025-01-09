4fbaf4eaf137f44c713860b0d13f599240806586
Will Peel <william.e.peel@icloud.com>
2025-01-09 22:05:32 +0000

    Added lakes that buildings cannot be placed on

--- game/core/src/main/java/io/github/archessmn/eng1/GameScreen.java
+++ game/core/src/main/java/io/github/archessmn/eng1/GameScreen.java
@@ -49,6 +49,7 @@ public class GameScreen implements Screen {
     private Vector2 unprojectedTouchPos = new Vector2();
 
     private Array<Building> draggablebuildings = new Array<>();
+    private Array<Lake> lakes = new Array<>();
 
     private Timer timer = new Timer(300, 100);
 
@@ -274,6 +275,7 @@ public class GameScreen implements Screen {
         }
 
         this.drawBuildingMenu();
+        world.drawLakes(batch);
 
         world.drawbuildings(batch);
 
