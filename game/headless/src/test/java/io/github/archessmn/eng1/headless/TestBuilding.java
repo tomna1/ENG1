16170f8064faaa3f59a55b12030c0bf1c6381235
Letam <letam4nigeria@gmail.com>
2025-01-12 17:15:51 +0000

    Added 2 tests for checking correct building type and creating buildings

--- game/headless/src/test/java/io/github/archessmn/eng1/headless/TestBuilding.java
+++ game/headless/src/test/java/io/github/archessmn/eng1/headless/TestBuilding.java
@@ -2,6 +2,7 @@ package io.github.archessmn.eng1.headless;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.github.archessmn.eng1.World;
@@ -9,10 +10,18 @@ import io.github.archessmn.eng1.buildings.Building;
 import io.github.archessmn.eng1.buildings.GymBuilding;
 
 public class TestBuilding extends AbstractHeadlessGdxTest {
+    private World world;
+
+    @BeforeEach
+    void init() {
+        world = new World(100, 100); // use a test world.
+    }
+
     @Test
+    // This tests checks that when a building is created it is assigned the corect
+    // type.
     public void TestCorrectBuildingType() {
 
-        World world = new World(100, 100); // use a test world.
         GymBuilding gymBuilding = new GymBuilding(world); // Verify the building's type is set to GYM
         assertEquals(Building.Type.GYM, gymBuilding.getBuildingType());
         // when you have an instance of a GYM building
@@ -21,4 +30,5 @@ public class TestBuilding extends AbstractHeadlessGdxTest {
         // right building type
 
     }
+
 }
